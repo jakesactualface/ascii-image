fc8c4c37cdf6a8a0801d7dc0d0ce725d6041d6c7
Jake Everhart <jake.a.everhart@gmail.com>
2023-07-29 19:23:09 -0500

    Enables file retrieval from Windows clipboard

--- src/scripts/image_from_clipboard.ps1
+++ src/scripts/image_from_clipboard.ps1
@@ -1,4 +1,14 @@
 $stream = New-Object System.IO.MemoryStream
 $img = Get-Clipboard -Format Image
-$img.save($stream, [System.Drawing.Imaging.ImageFormat]::Png)
-[System.Convert]::ToBase64String($stream.ToArray())
+if ( $img -eq $null )
+{
+	$filename = Get-Clipboard -Format FileDropList -Raw
+	$bytes = [System.IO.File]::ReadAllBytes($filename)
+	$stream = [System.IO.MemoryStream]::new($bytes)
+	[System.Convert]::ToBase64String($stream.ToArray())
+}
+else
+{
+	$img.save($stream, [System.Drawing.Imaging.ImageFormat]::Png)
+	[System.Convert]::ToBase64String($stream.ToArray())
+}
